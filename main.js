e63dd0d4b3dda51fda9d7266a8a807be53553875
nigelde <130147072+nigelde@users.noreply.github.com>
2024-09-28 17:11:00 +0100

    New pieces added

--- main.js
+++ main.js
@@ -8,14 +8,11 @@ $(document).ready(() => {
     $(".bio-img").hide();
         $(".bio-img").fadeIn(1500);
 
-    $(".painting-box").on("click", (event) => {
+    $(".painting-div").on("click", (event) => {
         $(event.currentTarget).toggleClass("painting-box-active");
     });
-
     
-    $(".enlarge").on("click", function() {
-        $(this).html() == "Exit" ? $(this).html('Enlarge') : $(this).html('Exit');
-    });
+   
 
         
     
